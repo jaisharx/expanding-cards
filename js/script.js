e740c3f91d5c186a6d90c48e1925ed5ff31a3f16
jai-sharma <sendtojsharma@gmail.com>
2021-01-22 22:22:30 +0530

    added click logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// type your js logic here...
+const panels = document.querySelectorAll('.panel');
+
+const switchActiveClass = (panelProvided) => {
+    panels.forEach((panel) => {
+        if (panel.classList.contains('active')) {
+            panel.classList.remove('active');
+        }
+    });
+
+    // set the active class to given panel
+    panelProvided.classList.add('active');
+};
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        switchActiveClass(panel);
+    });
+});
